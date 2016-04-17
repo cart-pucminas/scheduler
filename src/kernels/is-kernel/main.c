0f3c5a8f09c5235246203d1db1181e96ecbd3a84
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-04-16 22:13:49 -0300

    adding copyright

--- src/kernels/is-kernel/main.c
+++ src/kernels/is-kernel/main.c
@@ -1,4 +1,44 @@
-
+/*************************************************************************
+ *                                                                       * 
+ *       N  A  S     P A R A L L E L     B E N C H M A R K S  3.3        *
+ *                                                                       *
+ *                      O p e n M P     V E R S I O N                    *
+ *                                                                       * 
+ *                                  I S                                  * 
+ *                                                                       * 
+ ************************************************************************* 
+ *                                                                       * 
+ *   This benchmark is an OpenMP version of the NPB IS code.             *
+ *   It is described in NAS Technical Report 99-011.                     *
+ *                                                                       *
+ *   Permission to use, copy, distribute and modify this software        *
+ *   for any purpose with or without fee is hereby granted.  We          *
+ *   request, however, that all derived work reference the NAS           *
+ *   Parallel Benchmarks 3.3. This software is provided "as is"          *
+ *   without express or implied warranty.                                *
+ *                                                                       *
+ *   Information on NPB 3.3, including the technical report, the         *
+ *   original specifications, source code, results and information       *
+ *   on how to submit new results, is available at:                      *
+ *                                                                       *
+ *          http://www.nas.nasa.gov/Software/NPB/                        *
+ *                                                                       *
+ *   Send comments or suggestions to  npb@nas.nasa.gov                   *
+ *                                                                       *
+ *         NAS Parallel Benchmarks Group                                 *
+ *         NASA Ames Research Center                                     *
+ *         Mail Stop: T27A-1                                             *
+ *         Moffett Field, CA   94035-1000                                *
+ *                                                                       *
+ *         E-mail:  npb@nas.nasa.gov                                     *
+ *         Fax:     (650) 604-3957                                       *
+ *                                                                       *
+ ************************************************************************* 
+ *                                                                       * 
+ *   Author: M. Yarrow                                                   * 
+ *           H. Jin                                                      * 
+ *                                                                       * 
+ *************************************************************************/
 
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
