70f67a6a2f04c00733a4aa0b695a526848298a47
Pedro H. Penna <pedrohenriquepenna@gmail.com>
2016-12-01 17:39:56 -0200

    workload

---
+++ include/workload.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright(C) 2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * This file is part of WorkloadGen.
+ *
+ * WorkloadGen is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * WorkloadGen is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WorkloadGen; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef WORKLOAD_H_
+#define WORKLOAD_H_
+
+	#include <stdio.h>
+
+	#include "statistics.h"
+
+	/**
+	 * @brief Opaque pointer to a synthetic workload.
+	 */
+	typedef struct workload * workload_tt;
+
+	/**
+	 * @brief Constant opaque pointer to a synthetic workload.
+	 */
+	typedef const struct workload * const_workload_tt;
+
+	/**
+	 * @brief Workload sorting types.
+	 */
+	enum workload_sorting
+	{
+		ASCENDING,  /**< Ascending order.  */
+		DESCENDING, /**< Descending order. */
+		SHUFFLE     /**< Shuffle.          */
+	};
+
+	/**
+	 * @name Operations on Workload
+	 */
+	/**@{*/
+	extern workload_tt workload_create(histogram_tt, int);
+	extern void workload_destroy(workload_tt);
+	extern void workload_sort(workload_tt, enum workload_sorting);
+	extern void workload_write(FILE *, const_workload_tt);
+	extern workload_tt workload_read(FILE *);
+	/**@}*/
+
+#endif /* WORKLOAD_H_ */
---
+++ src/workload.c
@@ -0,0 +1,252 @@
+/*
+ * Copyright(C) 2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
+ * 
+ * This file is part of WorkloadGen.
+ *
+ * WorkloadGen is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * WorkloadGen is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with WorkloadGen; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+
+#include <statistics.h>
+#include <workload.h>
+#include <util.h>
+
+/**
+ * @brief Synthetic workload.
+ */
+struct workload
+{
+	int ntasks; /**< Number of tasks. */
+	int *tasks; /**< Tasks.           */
+};
+
+/**
+ * @brief Creates a workload.
+ *
+ * @param h   Histogram of probability distribution.
+ * @param ntasks Number of tasks.
+ */
+struct workload *workload_create(histogram_tt h, int ntasks)
+{
+	int k;              /* Residual tasks. */
+	struct workload *w; /* Workload.       */
+
+	/* Sanity check. */
+	assert(h != NULL);
+	assert(ntasks > 0);
+
+	/* Create workload. */
+	w = smalloc(sizeof(struct workload));
+	w->ntasks = ntasks;
+	w->tasks = smalloc(ntasks*sizeof(int));
+
+	/* Create workload. */
+	k = 0;
+	for (int i = 0; i < histogram_nclasses(h); i++)
+	{
+		int n;
+
+		n = floor(histogram_class(h, i)*ntasks);
+
+		for (int j = 0; j < n; j++)
+			w->tasks[k++] = (i + 1);
+	}
+
+	/* Fill up remainder tasks with minimum load. */
+	for (int i = k; i < ntasks; i++)
+		w->tasks[k++] = 1;
+
+	return (w);
+}
+
+/**
+ * @brief Destroys a workload.
+ *
+ * @param w Target workload.
+ */
+void workload_destroy(struct workload *w)
+{
+	/* Sanity check. */
+	assert(w != NULL);
+
+	free(w->tasks);
+	free(w);
+}
+
+/**
+ * @brief Sorts tasks in ascending order.
+ *
+ * @oaram w Target workload.
+ */
+static void workload_ascending(struct workload *w)
+{
+	/* Sanity check. */
+	assert(w != NULL);
+
+	for (int i = 0; i < w->ntasks; i++)
+	{
+		for (int j = 0; j < w->ntasks; j++)
+		{
+			if (w->tasks[j] < w->tasks[i])
+			{
+				double tmp;
+
+				tmp = w->tasks[j];
+				w->tasks[j] = w->tasks[i];
+				w->tasks[i] = tmp;
+			}
+		}
+	}
+}
+
+/**
+ * @brief Sorts tasks in descending order.
+ *
+ * @oaram w Target workload.
+ */
+static void workload_descending(struct workload *w)
+{
+	/* Sanity check. */
+	assert(w != NULL);
+
+	for (int i = 0; i < w->ntasks; i++)
+	{
+		for (int j = 0; j < w->ntasks; j++)
+		{
+			if (w->tasks[j] > w->tasks[i])
+			{
+				double tmp;
+
+				tmp = w->tasks[j];
+				w->tasks[j] = w->tasks[i];
+				w->tasks[i] = tmp;
+			}
+		}
+	}
+}
+
+/**
+ * @brief Shuffle tasks.
+ *
+ * @oaram w Target workload.
+ */
+static void workload_shuffle(struct workload *w)
+{
+	static unsigned seed;
+
+	/* Sanity check. */
+	assert(w != NULL);
+
+	seed = time(NULL);
+
+	/* Shuffle array. */
+	for (int i = 0; i < w->ntasks - 1; i++)
+	{
+		int j;      /* Shuffle index.  */
+		double tmp; /* Temporary data. */
+
+		j = rand_r(&seed)%w->ntasks;
+
+		tmp = w->tasks[i];
+		w->tasks[i] = w->tasks[j];
+		w->tasks[j] = tmp;
+	}
+}
+
+/**
+ * @brief Sorts tasks.
+ *
+ * @param w       Target workload.
+ * @param sorting Sorting type.
+ */
+void workload_sort(struct workload *w, enum workload_sorting sorting)
+{
+	/* Sanity check. */
+	assert(w != NULL);
+
+	/* Sort workload. */
+	switch(sorting)
+	{
+		/* Sort in ascending order. */
+		case ASCENDING:
+			workload_ascending(w);
+			break;
+
+		/* Sort in descending order. */
+		case DESCENDING:
+			workload_descending(w);
+			break;
+
+		/* Shuffle workload. */
+		case SHUFFLE:
+			workload_shuffle(w);
+			break;
+
+		/* Should not happen. */
+		default:
+			assert(0);
+			break;
+	}
+}
+
+/**
+ * @brief Writes a workload to a file.
+ *
+ * @param outfile Output file.
+ * @param w       Target workload.
+ */
+void workload_write(FILE *outfile, const struct workload *w)
+{
+	/* Sanity check. */
+	assert(outfile != NULL);
+	assert(w != NULL);
+
+	/* Write workload to file. */
+	fprintf(outfile, "%d\n", w->ntasks);
+	for (int i = 0; i < w->ntasks; i++)
+		fprintf(outfile, "%d\n", w->tasks[i]);
+}
+
+/**
+ * @brief Reads a workload from a file.
+ *
+ * @param infile Input file.
+ *
+ * @returns A workload.
+ */
+struct workload *workload_read(FILE *infile)
+{
+	int ntasks;         /**< Number of tasks. */
+	struct workload *w; /**< Workload.        */
+
+	/* Sanity check. */
+	assert(infile != NULL);
+
+	assert(fscanf(infile, "%d\n", &ntasks) == 1);
+
+	w = smalloc(ntasks);
+
+	/* Write workload to file. */
+	for (int i = 0; i < ntasks; i++)
+		assert(fscanf(infile, "%d\n", &w->tasks[i]) == 1);
+
+	return (w);
+}
